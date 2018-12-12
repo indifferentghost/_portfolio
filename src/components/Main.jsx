be6952527b226a13ef37bb184495e33bd1cfafd8
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-12-12 16:24:13 -0700

    testing forms.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -98,7 +98,8 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="bot-field" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
