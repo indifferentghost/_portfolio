6df7f4b8bcc744fc809da78045e6f40b5bb07a46
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-12-13 15:10:29 -0700

    added 1.0.0 tag, fixed spelling errors.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -103,7 +103,7 @@ class Main extends React.Component {
             </span>
             <section style={{ padding: '0 10px' }}>
               <p style={{ display: 'inline-block' }}>
-                An individual, self-starter always improving. Passionate about
+                A self-starter always improving. Passionate about
                 open source. I have been a student and teacher for web development.
                 At the moment I have been bewitched by React!
                 By the way, check out my <a href="#projects">awesome work</a>.
@@ -113,7 +113,7 @@ class Main extends React.Component {
                 realizing the impenetrable glass ceiling preventing my progression
                 I changed careers with a little help from Lambda School. I am hungry
                 and passionate about computer science and development of software,
-                and will continue to grow, learn and develop even inspite of blockers
+                and will continue to grow, learn and develop even in spite of blockers
                 in my way.
               </p>
             </section>
